c7daecd313e0e14ba42e7e70ff24fbeaf789b3bb
Satvik Virmani <70750314+SatvikVirmani@users.noreply.github.com>
2021-05-23 13:58:20 +0530

    Initial Commit✔

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="playmedia",
+    version="0.0.2",
+    author="Satvik Virmani",
+    author_email="virmanisatvik01@gmail.com",
+    description="A python package to play and control media files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SatvikVirmani/playmedia",
+    project_urls={
+        "Bug Tracker": "https://github.com/SatvikVirmani/playmedia/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.0",
+    install_requires=[
+        'python-vlc'
+    ]
+)
